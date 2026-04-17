fix bugs and add psnr star calculation

lib/models/street_gaussian_model.py
@@ -449,7 +449,7 @@ class StreetGaussianModel(nn.Module):
         return opacities
             
     def get_covariance(self, scaling_modifier = 1):
-        scaling = self.get_scoaling # [N, 1]
+        scaling = self.get_scaling # [N, 1]
         rotation = self.get_rotation # [N, 4]
         L = build_scaling_rotation(scaling_modifier * scaling, rotation)
         actual_covariance = L @ L.transpose(1, 2)
@@ -473,7 +473,7 @@ class StreetGaussianModel(nn.Module):
             obj_rot = quaternion_to_matrix(obj_rot.unsqueeze(0)).squeeze(0)
             
             normals_obj_global = normals_obj_local @ obj_rot.T
-            normals_obj_global = torch.nn.functinal.normalize(normals_obj_global)                
+            normals_obj_global = torch.nn.functional.normalize(normals_obj_global)                
             normals.append(normals_obj_global)
 
         normals = torch.cat(normals, dim=0)

lib/utils/waymo_utils.py
@@ -164,7 +164,7 @@ def get_obj_pose_tracking(datadir, selected_frames, ego_poses, cameras=[0, 1, 2,
             objects_info[track_id]['length'] = max(objects_info[track_id]['length'], float(tracklet[6]))
             
         tr_array = np.concatenate(
-            [np.array(tracklet[:2]).astype(np.float64), np.array([type]), np.array(tracklet[4:]).astype(np.float64)]
+            [np.array(tracklet[:2]).astype(np.float64), np.array([waymo_track2label[object_class]]), np.array(tracklet[4:]).astype(np.float64)]
         )
         tracklets_ls.append(tr_array)
         n_obj_in_frame[frame_id] += 1